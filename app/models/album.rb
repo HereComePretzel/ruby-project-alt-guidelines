6495efa9aae57dc6b7b35a0ec0b9a38b7a74af24
github username <github email address>
2020-08-18 17:56:02 -0500

    ok

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,30 +1,31 @@
 class Album < ActiveRecord::Base
     has_many :songs
     has_many :artists, through: :songs
-    def find_by_album_title(album_title)
-        self.all.select do |album_instance|
-            album_instance.album_title == self
-            binding.pry
-        end
-    end
+end 
+#     def find_by_album_title(album_title)
+#         self.all.select do |album_instance|
+#             album_instance.album_title == self
+#             binding.pry
+#         end
+#     end
 
-    def find_by_creation_year(creation_year)
-        self.all.select do |album_instance|
-            album_instance.creation_year == self
-        end
-    end
+#     def find_by_creation_year(creation_year)
+#         self.all.select do |album_instance|
+#             album_instance.creation_year == self
+#         end
+#     end
 
-    def find_by_genre(genre)
-        self.all.select do |album_instance|
-            album_instance.genre == self
-        end
-    end
+#     def find_by_genre(genre)
+#         self.all.select do |album_instance|
+#             album_instance.genre == self
+#         end
+#     end
 
-    def find_by_artist(artist)
-        self.all.select do |album_instance|
-            album_instance.name == self
-        end 
-    end 
+#     def find_by_artist(artist)
+#         self.all.select do |album_instance|
+#             album_instance.name == self
+#         end 
+#     end 
 
-end # of class 
+# end # of class 
 
