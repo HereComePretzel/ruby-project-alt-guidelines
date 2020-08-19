edbab15d093b9348155e25b98ca88bc5cbe51629
github username <github email address>
2020-08-19 10:45:55 -0500

    updated run

--- bin/run.rb
+++ bin/run.rb
@@ -84,5 +84,92 @@ when "2"
             #updates album info
         end 
     end 
+    # when "3"
+    #     puts "1. Browse by Artist"
+    #     puts "2. Browse by Album"
+    #     puts "3. Browse by genre"
+    #     puts "4. Browse by release year"
+    #     print "Enter your selection: "
+    #     input = gets.chomp
+    #         case input
+    #         when "1"
+    #             puts "Enter artists' name: "
+    #             gets.chomp
+    #             #3
+    #         when "2"
+    #             puts "Enter genre: "
+    #             gets.chomp
+    #             #returns all album with genre
+    #         when "3"
+    #             puts "Enter release year: "
+    #             gets.chomp
+    #             #returns all albums with release year
+    #         end 
+    
+end 
+
+#         case input
+#         when "1"
+#         puts "Enter artist name: "
+#         gets.chomp
+#         #find album by artist name
+#     when "2"
+#         puts "Enter album name: "
+#         gets.chomp
+#         #returns albums by album name
+#     when "3"
+#         puts "Enter genre: "
+#         gets.chomp
+#         #returns albums by genre
+#     when "4"
+#         puts "Enter release year(YYYY): "
+#         gets.chomp
+#         #returns albums by release year
+#     end 
+# case input_modify
+# when "2"
+
+# puts "1. View your profile"
+# puts "2. View your songs"
+# puts "3. Create a song"
+# puts "4. Remove a song"
+
+# print "Enter your selection: "
+# input = gets.chomp
+
+# # clear!
+
+# case input
+# when "1"
+#     puts "Profile page"
+# when "2"
+#     puts "Here are all your songs: "
+#     current_user.songs.each do |song|
+#         puts "#{song.album.album_title} | #{song.song_title}"
+#     end
+# when "3"
+#     puts "Add your song"
+#     puts "Please see the list of available albums: "
+#     Album.all.each do |album|
+#         puts "#{album.id}. #{album.album_title}"
+#     end
+
+#     print "Please enter your album id: "
+#     album_id = gets.chomp.to_i
+
+#     print "Please enter song title: "
+#     song_title = gets.chomp
+
+#     Song.create(
+#         song_title: song_title,
+#         artist_id: current_user.id,
+#         album_id: album_id
+#     )
+
+# when "4"
+#     puts "Remove your song"
+# end
+
+# #binding.pry
 
 # binding.pry
