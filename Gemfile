396462cc6a71fde764a36214380b249b6d635668
github username <github email address>
2020-08-20 15:29:58 -0500

    new

--- Gemfile
+++ Gemfile
@@ -9,4 +9,4 @@ gem "pry"
 gem "require_all"
 gem "faker"
 gem 'ruby_afplay'
-gem 'tty'
+gem 'tty-prompt'
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 require_relative '../config/environment'
 require_relative '../config/command_line_interface'
+require 'tty-prompt'
+prompt = TTY::Prompt.new
 ActiveRecord::Base.logger = nil
 # player = RubyAfplay::Player.new("./bin/parlor.mp3", volume: 2, time: 30, rate: 1)
 # player.play
@@ -18,48 +20,41 @@ clear!
 
 current_user = Artist.find_or_create_by(name: artist_name)
 
-# puts "Welcome #{current_user.name}"
-# puts "What would you like to do next?"
-# puts "1. Search Collection"
-# puts "2. Modify Collection"
-# puts "3. View Profile"
-# print "Enter your selection: "
-# input = gets.chomp
-
 clear!
 
-puts "Hey there, #{current_user.name}!"
+puts "You ready to get down, #{current_user.name}?"
 main_menu
 input = gets.chomp
 clear!
 
 while input != "Exit"
-  case input
+    case input
     when "1"
-        puts "How would you like to search?"
-        puts "1. Artist"
-        puts "2. Album"
-        puts "3. Genre"
-        puts "4. Release year"
-        puts "5. Song Title"
-        print "Enter your selection: "
-        input = gets.chomp
+        prompt = prompt.select("\nHow would you like to search?", ["Artist", "Album", "Genre", "Release Year"])
+        # puts "How would you like to search?"
+        # puts "1. Artist"
+        # puts "2. Album"
+        # puts "3. Genre"
+        # puts "4. Release year"
+        # puts "5. Song Title"
+        # print "Enter your selection: "
+        # input = gets.chomp
         clear!
-            case input
-            when "1"
-                puts "Enter Artists' name: "
+            
+            if prompt == "Artist"
+                puts "Enter Artist name: "
                 artist_name = gets.chomp 
                 found_artist = Artist.find_by(name: artist_name)
                 found_artist.albums.map {|album_instance| puts "#{album_instance.album_title} | #{album_instance.genre} | #{album_instance.creation_year}"} 
             
-            when "2"
+            elsif "2"
                 puts "Enter Album name: "
                 album_title = gets.chomp
                 xyz = Album.where(album_title: album_title)
                 xyz.map {|album_instance| puts "#{album_instance.album_title} | #{album_instance.genre} | #{album_instance.creation_year}"}
                 
             
-            when "3"
+            elsif "3"
                 puts "Enter genre name: "
                 genre = gets.chomp
                 found_album = Album.where(genre: genre)
@@ -70,7 +65,7 @@ while input != "Exit"
                 #tty table
                 
                 
-            when "4"
+            else "4"
                 puts "Enter release year(YYYY): "
                 creation_year = gets.chomp
                 a = Album.where(creation_year: creation_year)
@@ -82,13 +77,9 @@ while input != "Exit"
             #     song_title = gets.chomp
             #     found_song = Song.where(song_title: song_title)
             #     found_song.each {|song_instance| puts "#{song_instance.album_songs}|#{song_instance.genre}|#{song_instance.creation_year}"}
-    
+            end 
             
-            
-            
-    
-
-    when "2"
+        when "2"
     options
     select
     answer = gets.chomp
@@ -106,7 +97,7 @@ while input != "Exit"
                     add_album
                     exit_and_menu
                 else
-                    puts "Please select your next step: "
+                    puts "Where to now? "
                     exit_and_menu
                 end
                 
